912262a93f50c17e56f248921993a170f7182956
keithguske <keith@guske.net>
2015-10-17 17:56:01 -0400

    firstCommit
    best commit ever

---
+++ Scripts/Player1Movement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour {
+	// Use this for initialization
+	private Rigidbody2D rb;
+	void Start () {
+		rb = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("a"))
+		{
+			rb.AddForce(new Vector2(-1.0f, 0));
+			print("yes");
+
+		}
+		else if(Input.GetKey("d"))
+		{
+			rb.AddForce(new Vector2(1.0f, 0));
+			//transform.position += new Vector3(1f, 0f, 0);
+		}
+		else if(Input.GetKey("s"))
+		{
+			Debug.Log("This is the down button, it does nothing");
+			//transform.position += new Vector3(0, -1f, 0);
+		}
+		else if(Input.GetKey("w"))
+		{
+			rb.AddForce(new Vector2(0, 3.0f));
+			//transform.position += new Vector3(0, 1f, 0);
+		}
+	}
+}
---
+++ Scripts/Player2Movement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Movement : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("left"))
+		{
+			transform.position += new Vector3(-1f, 0f, 0);
+		}
+		else if(Input.GetKeyDown("right"))
+		{
+			transform.position += new Vector3(1f, 0f, 0);
+		}
+		else if(Input.GetKeyDown("down"))
+		{
+			transform.position += new Vector3(0, -1f, 0);
+		}
+		else if(Input.GetKeyDown("up"))
+		{
+			transform.position += new Vector3(0, 1f, 0);
+		}
+	}
+}

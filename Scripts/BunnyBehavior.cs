92fd5f0c6a02685f13671f6899ff40356b20cc9b
keithguske <keith@guske.net>
2015-10-18 11:46:58 -0400

    One Commit to Rule Them All
    This will allow bunny attacks

---
+++ Scripts/BunnyBehavior.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BunnyBehavior : MonoBehaviour {
+	public GameObject player1;
+	public GameObject player2;
+	public GameObject heldBy;
+	private PlayerInfo player1Info;
+	private PlayerInfo player2Info;
+	
+
+	// Use this for initialization
+	void Start () {
+		player1 = GameObject.Find("Player1");
+		player2 = GameObject.Find("Player2");
+		player1Info = player1.GetComponent<PlayerInfo>();
+		player2Info = player2.GetComponent<PlayerInfo>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		print("2 Bunnies just collided");
+	}
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if(collision.name == "Player1" && heldBy.name == "Player2")
+		{
+			player1Info.balloonAmount -= 1;
+			print ("Player1 just lost a balloon");
+		}
+		if(collision.name == "Player2" && heldBy.name == "Player1")
+		{
+			player2Info.balloonAmount -= 1;
+			print ("Player2 just lost a balloon");
+		}
+	}	
+}
--- Scripts/Player1Movement.cs
+++ Scripts/Player1Movement.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player1Movement : MonoBehaviour {
 	// Use this for initialization
 	private Rigidbody2D rb;
+<<<<<<< HEAD
 
 	public float cooldown = .55f;
 
@@ -11,30 +12,52 @@ public class Player1Movement : MonoBehaviour {
 
 	//Sprivate Animator
 
+=======
+	private GameObject bunny;
+	private BunnyBehavior bunnyScript;
+	private PlayerInfo playerInfo;
+>>>>>>> 0ee87d967b299d04ddc2c5a2c35a41e70a3a1a39
 
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
+		playerInfo = this.gameObject.GetComponent<PlayerInfo>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetKey("a"))
 		{
+<<<<<<< HEAD
 
 			rb.AddForce(new Vector2(-1.0f, 0));
 			print("yes");
 
 		}
 		if(Input.GetKey("d"))
-		{
-			rb.AddForce(new Vector2(1.0f, 0));
-			//transform.position += new Vector3(1f, 0f, 0);
+=======
+			rb.AddForce(new Vector2(-2.0f, 0));
+			transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
+
 		}
+		if(Input.GetKey("d"))
+		{
+			rb.AddForce(new Vector2(2.0f, 0));
+			transform.localScale = new Vector3(-1, transform.localScale.y, transform.localScale.z);
+		}
+		//this is the down button, it does nothing
 		if(Input.GetKey("s"))
+>>>>>>> 0ee87d967b299d04ddc2c5a2c35a41e70a3a1a39
 		{
-			Debug.Log("This is the down button, it does nothing");
-			//transform.position += new Vector3(0, -1f, 0);
 		}
+<<<<<<< HEAD
+		if(Input.GetKey("s"))
+=======
+		if(Input.GetKey("w") && playerInfo.balloonAmount>0)
+>>>>>>> 0ee87d967b299d04ddc2c5a2c35a41e70a3a1a39
+		{
+			rb.AddForce(new Vector2(0, 4.0f));
+		}
+<<<<<<< HEAD
 		if(Time.time >= timeStamp && Input.GetKey("w"))
 		{
 			rb.AddForce(new Vector2(0, 30f));
@@ -42,6 +65,17 @@ public class Player1Movement : MonoBehaviour {
 
 			timeStamp = Time.time + cooldown;
 			Debug.Log("here");
+=======
+		if(Input.GetKeyDown("f"))
+		{
+			bunny = Instantiate(Resources.Load("Prefabs/bunny") as GameObject);
+			bunny.transform.position = transform.position + new Vector3(0, 1f, 0);
+			bunny.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100*transform.localScale.x, 0));
+			bunny.transform.localScale = new Vector2(transform.localScale.x/2 * -1, bunny.transform.localScale.y);
+			bunnyScript = bunny.GetComponent<BunnyBehavior>();
+			bunnyScript.heldBy = this.gameObject;
+
+>>>>>>> 0ee87d967b299d04ddc2c5a2c35a41e70a3a1a39
 		}
 	}
 }

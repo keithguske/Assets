9b3e4d7c5d472fbfaf95af505b0e6deb5659de47
keithguske <keith@guske.net>
2015-10-17 20:54:52 -0400

    Second Commit
    game jam stuff

--- Scripts/Player1Movement.cs
+++ Scripts/Player1Movement.cs
@@ -4,6 +4,14 @@ using System.Collections;
 public class Player1Movement : MonoBehaviour {
 	// Use this for initialization
 	private Rigidbody2D rb;
+
+	public float cooldown = .55f;
+
+	private float timeStamp = 0;
+
+	//Sprivate Animator
+
+
 	void Start () {
 		rb = this.GetComponent<Rigidbody2D> ();
 	}
@@ -12,24 +20,28 @@ public class Player1Movement : MonoBehaviour {
 	void Update () {
 		if(Input.GetKey("a"))
 		{
+
 			rb.AddForce(new Vector2(-1.0f, 0));
 			print("yes");
 
 		}
-		else if(Input.GetKey("d"))
+		if(Input.GetKey("d"))
 		{
 			rb.AddForce(new Vector2(1.0f, 0));
 			//transform.position += new Vector3(1f, 0f, 0);
 		}
-		else if(Input.GetKey("s"))
+		if(Input.GetKey("s"))
 		{
 			Debug.Log("This is the down button, it does nothing");
 			//transform.position += new Vector3(0, -1f, 0);
 		}
-		else if(Input.GetKey("w"))
+		if(Time.time >= timeStamp && Input.GetKey("w"))
 		{
-			rb.AddForce(new Vector2(0, 3.0f));
+			rb.AddForce(new Vector2(0, 30f));
 			//transform.position += new Vector3(0, 1f, 0);
+
+			timeStamp = Time.time + cooldown;
+			Debug.Log("here");
 		}
 	}
 }
